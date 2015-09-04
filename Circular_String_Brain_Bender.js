a639fb099914d574546efefb52a9beff905d2a76
BraddahBoots <bryanjb@gmail.com>
2015-09-04 11:08:45 -1000

    Pushing code from 9am brain buster

---
+++ Circular_String_Brain_Bender.js
@@ -0,0 +1,23 @@
+'strict mode';
+
+  // Creat a function
+ function circString( source, find ) {
+  var sourceWord = str.concat(source + source);
+  if(sourceWord.indexOf(find)) {
+      return true;
+  }
+}
+
+console.log(circString(Hotdog, dog);
+
+
+  // Invokes an algorithm
+  // Algorithm takes 2 string ( source, find )
+  // Function should return "true" if the string being passed through ( find )...
+  // is found in the ( source )
+  // Only if ( source ) is circular in nature = there is no end to the ( source ) string
+  // A match (between ( find ) and ( source ) is true if the word to find is...
+  // partially (look up) at the end of the word and at the beginning in sequence.
+
+  // Ex ( souce = Hotdog, find = dog --- true ) ( source = batman, find = bat --- true)
+
